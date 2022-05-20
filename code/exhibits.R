a776359b6fd66aa1c65fa994016087ce37efe569
Ofek-Shanny <aw37abig@fauad.fau.de>
2022-05-20 13:09:18 +0200

    updated exhibits

--- code/exhibits.R
+++ code/exhibits.R
@@ -2,6 +2,9 @@ setwd("~/D01591732748000314893/C/Users/Yuval/OneDrive - University of Haifa/data
 library(tidyverse)
 library(data.table)
 library(stargazer)
+library(gridExtra)
+library(gt)
+library(glue)
 
 clean_data <- readRDS("./data/clean_data.RDS")
 
@@ -10,25 +13,108 @@ clean_data[, treatment:=factor(treatment, levels = c("luck", "merit", "inheritan
 x_fig <- 20
 y_fig <- 10
 
+x_paper <- 15
+y_paper <- 15
+
+
+theme_update(plot.title = element_text(hjust = 0.5))
+
 # Descriptive statistics --------------------------------------------------
 
-t(clean_data[, .(mean_age=round(mean(age), 0), median_age=median(age), min_age=min(age), max_age=max(age), 
+table1 <- clean_data[, .('Mean age'=round(mean(age), 0), 'Median age'=median(age), 'Min age'=min(age), 'Max age'=max(age), 
                  'High Income' = sum(income_label=="High Income", na.rm = T)/.N,
                  'Average Income' = sum(income_label=="Average Income", na.rm = T)/.N,
                  'Low Income' = sum(income_label=="Low Income", na.rm = T)/.N,
                  'Unreported Income' = sum(is.na(income_label))/.N,
                  'High Education' = sum(education %in% c(5,6), na.rm = T)/.N,
                  'Female' = sum(female)/.N,
-                 Observations = .N), treatment])
+                 Observations = .N), treatment]
 
 
-# Figure 1 - Distribution of redistribution -------------------------------
-clean_data[, .N, .(treatment, redistribution)][, .(redistribution, Prop = N/sum(N)), treatment] %>% 
-  ggplot(mapping = aes(redistribution, Prop, fill = treatment)) + geom_bar(stat = "identity", position = "dodge") + 
-  scale_x_continuous(breaks = c(0, 2, 4, 6, 8, 10, 12)) + labs(title = "Figure 1 - Distribution of redistribution")
+table1a <- cbind(data.table(treatment = "All"), clean_data[, .('Mean age'=round(mean(age), 0), 'Median age'=median(age), 'Min age'=min(age), 'Max age'=max(age), 
+               'High Income' = sum(income_label=="High Income", na.rm = T)/.N,
+               'Average Income' = sum(income_label=="Average Income", na.rm = T)/.N,
+               'Low Income' = sum(income_label=="Low Income", na.rm = T)/.N,
+               'Unreported Income' = sum(is.na(income_label))/.N,
+               'High Education' = sum(education %in% c(5,6), na.rm = T)/.N,
+               'Female' = sum(female)/.N,
+               Observations = .N)])
 
-ggsave(filename = "./exhibits/figure_1_dist_of_redist.jpg", device = "jpeg", units = "cm", width = x_fig, height = y_fig)
+table1 <- rbind(table1a, table1)
 
+table2 <- t(table1[,-c("treatment")])
+colnames(table2) <- table1$treatment
+table2 <- as.data.frame(table2)
+
+setnames(table2, old = c("luck", "merit", "inheritance"), new = c("Luck", "Merit", "Inheritance"))
+
+stargazer(table2, type = "html", title = "Experiment population", style = "AER", summary = F, 
+          out = "./exhibits/Experiment_population.html", digits = 2)
+
+stargazer(table2, type = "latex", title = "Experiment population", style = "AER", summary = F, 
+          out = "./exhibits/Experiment_population.tex", digits = 2, label = "Experiment_population")
+
+`# Figure XXX - Distribution of redistribution -------------------------------
+clean_data[, choice:=factor(paste0("(", 12-redistribution, ",", redistribution, ")"), 
+                            levels = c("(12,0)", "(10,2)", "(8,4)", "(6,6)", "(4,8)", "(2,10)", "(0,12)"))]
+
+dist_4_plots <- function(specific_data, fig_name, yscale) {
+  all <- specific_data[, .N, .(choice)][, .(choice, Prop = N/sum(N))] %>% 
+    ggplot(mapping = aes(choice, Prop)) + geom_col() + scale_x_discrete(limits = c("(12,0)", "(10,2)", "(8,4)", "(6,6)", "(4,8)", "(2,10)", "(0,12)")) + 
+    labs(title = "Pooled results", x = "Choice", y = "Proportion") + theme_bw() + theme(plot.title = element_text(hjust = 0.5)) +
+    scale_y_continuous(limits = c(0, yscale))
+  
+  sub_group_hist <- function(treat_name){
+    fig <- specific_data[treatment==treat_name, .N, .(choice)][, .(choice, Prop = N/sum(N))] %>% 
+      ggplot(mapping = aes(choice, Prop)) + geom_col() + scale_x_discrete(limits = c("(12,0)", "(10,2)", "(8,4)", "(6,6)", "(4,8)", "(2,10)", "(0,12)")) +
+      labs(title = str_to_title(treat_name), x = "Choice", y = "Proportion") + theme_bw() + theme(plot.title = element_text(hjust = 0.5)) +
+      scale_y_continuous(limits = c(0, yscale))
+    return(fig)
+  }
+  
+  luck_fig <- sub_group_hist("luck")
+  merit_fig <- sub_group_hist("merit")
+  inheritance_fig <- sub_group_hist("inheritance")
+  
+  a <- grid.arrange(all, luck_fig, merit_fig, inheritance_fig)
+  
+  ggsave(plot = a, filename = paste0("./exhibits/figure_dist_of_choices_", fig_name, ".jpg"), device = "jpeg", units = "cm", width = x_paper, height = y_paper)
+  
+}
+
+dist_4_plots(clean_data, "All", yscale = 0.55)
+dist_4_plots(clean_data[`high income`==TRUE], "high_income", yscale = 0.8)
+dist_4_plots(clean_data[`high income`==FALSE], "not_high_income", yscale = 0.8)
+
+
+# Table XX1 & XX2 prop of choices ---------------------------------------------------------
+specific_data <- clean_data
+group_name <- "test"
+title_name <- "daf"
+prop_tables <- function(specific_data, title_name, group_name){
+  all_choice_table <- specific_data[, .N, applied_inequality][, c("treatment", "Proportion"):=.("pooled", N/sum(N))][, N:=sum(N), treatment][,applied_inequality:=as.character(round(applied_inequality, digits = 2))]
+  sub_group_choice_table <- specific_data[, .N, .(applied_inequality, treatment)][, Proportion:=N/sum(N), treatment][, N:=sum(N), treatment][,applied_inequality:=as.character(round(applied_inequality, digits = 2))]
+  tablexx <- rbind(all_choice_table, sub_group_choice_table) %>% dcast(...~applied_inequality, value.var = "Proportion")
+  tablexx <- tablexx[, .(treatment, `0`, `0.33`, `0.67`, `1`, N)]
+  tablexx[, treatment:=factor(treatment, levels = c("luck", "merit", "inheritance", "pooled"))]
+  setorder(tablexx, treatment)
+  
+  export_table <- gt(tablexx) %>% tab_header(title = title_name) %>% 
+    fmt_percent(columns = c("0", "0.33", "0.67", "1"), decimals = 1)
+  gtsave(export_table, filename = paste0("./exhibits/tablexx", group_name, ".html"))
+  gtsave(export_table, filename = paste0("./exhibits/tablexx", group_name, ".tex"))
+  
+}
+
+prop_tables(clean_data, title_name = "Applied Inequality Distribution", group_name = "1")
+prop_tables(clean_data[`high income`==TRUE], title_name = "Applied Inequality Distribution - High Income", group_name = "2high")
+prop_tables(clean_data[`high income`==FALSE], title_name = "Applied Inequality Distribution - not High Income", group_name = "2notHigh")
+
+
+# count_table <- clean_data[, .N, .(applied_inequality, treatment)]
+# tablexx1_count <- xtabs(N~treatment + applied_inequality, data = count_table)
+# chisq.test(tablexx1_count)
+# tablexx1_prop <- prop.table(tablexx1_count)
 
 # Figure 2 = mean applied inequality --------------------------------------
 
@@ -57,19 +143,26 @@ ggsave(filename = "./exhibits/figure_2A_applied_inequality_high_income.jpg", dev
 
 # Regression - Applied Inequality (Table 4) -------------------------------
 
-reg_app_inequality_no_controls <- lm(applied_inequality~merit + inheritance,data = clean_data)
-summary(reg_app_inequality_no_controls)
+#reg_app_inequality_no_controls
+m1  <- lm(applied_inequality~merit + inheritance,data = clean_data)
+summary(m1)
 
-reg_app_inequality_no_controls_clean <- lm(applied_inequality~merit + inheritance,data = clean_data, subset = !is.na(income_label))
-summary(reg_app_inequality_no_controls_clean)
+#reg_app_inequality_no_controls_clean
+m2 <- lm(applied_inequality~merit + inheritance,data = clean_data, subset = !is.na(income_label))
+summary(m2)
 
-
-reg_app_inequality <- lm(applied_inequality~merit + inheritance + age + `high education` + `high income` + female, data = clean_data)
+#reg_app_inequality
+m3 <- lm(formula = applied_inequality~merit + inheritance + age + `high education` + `high income` + female, data = clean_data)
 summary(reg_app_inequality)
 
-stargazer(reg_app_inequality_no_controls, reg_app_inequality_no_controls_clean, reg_app_inequality, type = "html", style = "AER",
-          title = "Table 4, Regression Results: Dependent variable: Applied Inequality", out = "./exhibits/reg_app_inequality.html",
-          omit.stat = c("f", "ser") )
+stargazer(m1, m2, m3, type = "html", style = "AER",
+          title = "Treatments Effect on Applied Inequality", out = "./exhibits/reg_app_inequality.html",
+          omit.stat = c("f", "ser"), dep.var.labels="")
+
+stargazer(m1, m2, m3, type = "latex", style = "AER",
+          title = "Treatments Effect on Applied Inequality", out = "./exhibits/reg_app_inequality.tex",
+          omit.stat = c("f", "ser"), label = "reg_app_inequality", dep.var.labels="")
+
 
 # Regression - Applied Inequality (Table 4) high income sub groups -------------------------------
 
@@ -80,18 +173,26 @@ reg_app_inequality_high_income <- lm(applied_inequality~merit + inheritance + ag
                          data = clean_data, subset = income_label=="High Income")
 summary(reg_app_inequality_high_income)
 
-reg_app_inequality_no_controls_not_high_income <- lm(applied_inequality~merit + inheritance,data = clean_data, subset = !is.na(income_label)&(income_label!="High Income"))
-summary(reg_app_inequality_no_controls_not_high_income)
+reg_app_inequality_no_controls_average_income <- lm(applied_inequality~merit + inheritance,data = clean_data, subset = !is.na(income_label)&(income_label=="Average Income"))
+summary(reg_app_inequality_no_controls_average_income)
 
 
-reg_app_inequality_not_high_income <- lm(applied_inequality~merit + inheritance + age + `high education` + female, 
-                                         data = clean_data, subset = !is.na(income_label)&(income_label!="High Income"))
-summary(reg_app_inequality_not_high_income)
+reg_app_inequality_average_income <- lm(applied_inequality~merit + inheritance + age + `high education` + female, 
+                                         data = clean_data, subset = !is.na(income_label)&(income_label=="Average Income"))
+summary(reg_app_inequality_average_income)
+
+reg_app_inequality_no_controls_low_income <- lm(applied_inequality~merit + inheritance,data = clean_data, subset = !is.na(income_label)&(income_label=="Low Income"))
+summary(reg_app_inequality_no_controls_low_income)
+
+reg_app_inequality_low_income <- lm(applied_inequality~merit + inheritance + age + `high education` + female, 
+                                        data = clean_data, subset = !is.na(income_label)&(income_label=="Low Income"))
+summary(reg_app_inequality_low_income)
 
 stargazer(reg_app_inequality_no_controls_high_income, reg_app_inequality_high_income, 
-          reg_app_inequality_no_controls_not_high_income, reg_app_inequality_not_high_income, type = "html", style = "AER",
-          title = "Table 4, Regression Results: Dependent variable: Applied Inequality, high income sub groups", out = "./exhibits/reg_app_inequality_income_sub.html",
-          omit.stat = c("f", "ser"), column.labels = c("High Income", "Not High Income"), column.separate = c(2, 2))
+          reg_app_inequality_no_controls_average_income, reg_app_inequality_average_income, reg_app_inequality_no_controls_low_income,
+          reg_app_inequality_low_income, type = "html", style = "AER",
+          title = "Table 4, Regression Results: Dependent variable: Applied Inequality, income sub groups", out = "./exhibits/reg_app_inequality_income_sub.html",
+          omit.stat = c("f", "ser"), column.labels = c("High Income", "Average Income", "Low Income"), column.separate = c(2, 2, 2))
 
 
 # Regression - Applied Inequality interactions (Table 5) -------------------------------
@@ -126,9 +227,14 @@ fairness_types <- rbind(fairness_types, list("Strong Meritocrats", Strong_Merito
 Aristocrats <- t.test(clean_data[treatment=="inheritance", redistribution<6], clean_data[treatment=="luck", redistribution<6])
 fairness_types <- rbind(fairness_types, list("Aristocrats", Aristocrats$estimate[[1]]-Aristocrats$estimate[[2]], Aristocrats$conf.int[[1]], Aristocrats$conf.int[[2]]))
 
+fairness_types[conf_low<0, conf_low:=0]
+
+fairness_types_US <- data.table(type = c("Egalitarians", "Libertarians", "Meritocrats"), estimate = c(0.153, 0.294, 0.375))
+
 fairness_types[, type:=factor(type, levels = c("Egalitarians", "Meritocrats", "Strong Meritocrats", "Aristocrats", "Libertarians"))]
+
 fairness_types %>% ggplot(mapping = aes(type, estimate)) + geom_bar(stat = "identity") + geom_errorbar(aes(ymin = conf_low, ymax = conf_high), width = 0.1) + 
-  labs(title = "Figure 4 - Percentage of Fairness Types")
+  labs(y = "Proportion") + theme_bw() + theme(text=element_text(size=15), axis.title.x = element_blank()) + geom_col(data = fairness_types_US, mapping = aes(type, estimate), alpha=0 ,color="red", size = 1)
 
 ggsave(filename = "./exhibits/figure_4_fairness_types.jpg", device = "jpeg", units = "cm", width = x_fig, height = y_fig)
 
@@ -140,3 +246,7 @@ ggsave(filename = "./exhibits/figure_4_fairness_types.jpg", device = "jpeg", uni
 clean_data %>% ggplot(mapping = aes(time_submit)) + geom_histogram(binwidth = 5) + geom_vline(xintercept = c(20), color = "red")
 clean_data[time_submit<20]
 clean_data[time_submit<20, mean(redistribution), treatment]
+
+clean_data[time_submit<150] %>% ggplot(mapping = aes(time_submit, redistribution, color=treatment)) + geom_jitter(width = 0.01, height = 0.2) + geom_smooth(se=F, )
+
+clean_data[time_submit<150] %>% ggplot(mapping = aes(time_submit, color = treatment)) + geom_boxplot()
